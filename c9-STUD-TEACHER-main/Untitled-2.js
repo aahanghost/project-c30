8832747dcddb263407d56159a44b2350a9191cc6
Aahan <73171258+aahanghost@users.noreply.github.com>
2021-11-01 16:57:02 +0530

    Add files via upload

---
+++ c9-STUD-TEACHER-main/Untitled-2.js
@@ -0,0 +1,23 @@
+
+
+var red = prompt('Enter the first variable: ');
+var green = prompt('Enter the second variable: ');
+function setup(){
+  var b1 = createButton("red");
+if(b1.mousePressed(swap);)
+background("red")
+  
+  var b2 = createButon("green");
+b2.mousePressed(swap);
+background("green")
+  }
+function draw()
+{
+ 
+}
+function swap()
+{
+  [red,green]=[green,red];
+  console.log("the value of red after swapping is:",+red);
+  console.log("the value of green after swapping is:",+green);
+}
